6720b4123e129fcb00e835fc26e2b113847120e8
Savagesteel <savagesteel@gmail.com>
2020-01-05 11:36:55 +0100

    Added base PowerShell scripts

---
+++ d1-dump-metadata.ps1
@@ -0,0 +1,178 @@
+
+<#
+
+.SYNOPSIS
+    Script to generate Diablo/Hellfire disc dump JSON metadata file.
+
+.DESCRIPTION
+    Script to generate Diablo/Hellfire disc dump JSON metadata file.
+
+.PARAMETER DumpPath
+    The folder containing the dump (Generated with d1-dump.ps1).
+
+#>
+Param
+(
+    [Parameter(Mandatory=$true,ValueFromPipeline=$false)]
+    [String]
+    $DumpPath
+)
+
+# Check dump folder and files
+if( -not (Test-Path -Path $DumpPath -PathType Container) )
+{
+    Write-Error -Message 'Dump folder not found. Exiting...'
+    exit
+}
+
+if( -not (Test-Path -Path $DumpPath\*.bin -PathType Leaf) `
+    -or -not (Test-Path -Path $DumpPath\*.cue -PathType Leaf) )
+{
+    Write-Error -Message 'Dump files not found. Exiting...'
+    exit
+}
+
+# Objects that will be used to generate the JSON metadata file
+$metadata = New-Object -TypeName System.Object
+$ringCodes = New-Object -TypeName System.Object
+
+# Gathering general information from dump folder name and user input
+try
+{
+    $dumpFolderName = Split-Path -Path $DumpPath -Leaf
+    $splitName = $dumpFolderName.Split('.')
+    $game = $($splitName[0])
+    $platform = $($splitName[1])
+    $region = $($splitName[2])
+}
+catch
+{
+    Write-Host -Object "Failed retrieving dump game, platform and region from dump folder name '$dumpFolderName'. Exiting..." -ForegroundColor Red
+    Write-Host -Object $_ -ForegroundColor Red
+    exit
+}
+
+$generalText = @"
+General Information
+-------------------
+
+Game: $game
+Platform: $platform
+Region: $region
+
+"@
+
+Write-Host -Object $generalText
+
+$metadata | Add-Member -MemberType NoteProperty -Name Game -Value $game
+$metadata | Add-Member -MemberType NoteProperty -Name Platform -Value $platform
+$metadata | Add-Member -MemberType NoteProperty -Name Region -Value $region
+
+if( $platform -eq 'PSX' )
+{
+    $metadata | Add-Member -MemberType NoteProperty -Name CountryCodeIsoAlpha2 -Value $null
+    $metadata | Add-Member -MemberType NoteProperty -Name SerialNumber `
+        -Value (Read-Host -Prompt 'Serial Number')
+}
+else
+{
+    $metadata | Add-Member -MemberType NoteProperty -Name CountryCodeIsoAlpha2 `
+        -Value (Read-Host -Prompt 'ISO 3166 Alpha 2 Country Code')
+    $metadata | Add-Member -MemberType NoteProperty -Name SerialNumber -Value $null
+}
+Write-Host -Object ''
+
+# Gathering ring codes from user input
+$ringCodesText = @"
+Ring Codes
+----------
+
+Example:
+  Mastering Code: AD/CA 24400 19 8 <8093> S6995710
+  IFPI Mastering SID Code: IFPI L488
+  Engraved Stamped Mastering Code: PP1
+  IFPI Mould SID Code: IFPI 8153
+
+"@
+
+Write-Host -Object $ringCodesText
+
+$masteringCode = Read-Host -Prompt 'Mastering Code'
+if( $masteringCode -eq '' )
+{
+    $masteringCode = $null
+}
+$ifpiMasteringSidCode = Read-Host -Prompt 'IFPI Mastering SID Code'
+if( $ifpiMasteringSidCode -eq '' )
+{
+    $ifpiMasteringSidCode = $null
+}
+$engravedStampedMasteringCode = Read-Host -Prompt 'Engraved Stamped Mastering Code'
+if( $engravedStampedMasteringCode -eq '' )
+{
+    $engravedStampedMasteringCode = $null
+}
+$ifpiMouldSidCode = Read-Host -Prompt 'IFPI Mould SID Code'
+if( $ifpiMouldSidCode -eq '' )
+{
+    $ifpiMouldSidCode = $null
+}
+
+$ringCodes | Add-Member -MemberType NoteProperty -Name MasteringCode -Value $masteringCode
+$ringCodes | Add-Member -MemberType NoteProperty -Name IfpiMasteringSidCode -Value $ifpiMasteringSidCode
+$ringCodes | Add-Member -MemberType NoteProperty -Name EngravedStampedMasteringCode -Value $engravedStampedMasteringCode
+$ringCodes | Add-Member -MemberType NoteProperty -Name IfpiMouldSidCode -Value $ifpiMouldSidCode
+
+$metadata | Add-Member -MemberType NoteProperty -Name RingCodes -Value $ringCodes
+
+Write-Host -Object ''
+
+# Compute checksums
+Write-Host -Object 'Generating checksums...'
+$checksums = @()
+
+$binHash = Get-FileHash -Path "$DumpPath\$Game.bin" -Algorithm SHA256
+$obj = New-Object -TypeName System.Object
+$obj | Add-Member -MemberType NoteProperty -Name FileName -Value "$Game.bin"
+$obj | Add-Member -MemberType NoteProperty -Name Algorithm -Value 'SHA256'
+$obj | Add-Member -MemberType NoteProperty -Name Hash -Value $binHash.Hash
+$checksums += $obj
+
+$cueHash = Get-FileHash -Path "$DumpPath\$Game.cue" -Algorithm SHA256
+$obj = New-Object -TypeName System.Object
+$obj | Add-Member -MemberType NoteProperty -Name FileName -Value "$Game.cue"
+$obj | Add-Member -MemberType NoteProperty -Name Algorithm -Value 'SHA256'
+$obj | Add-Member -MemberType NoteProperty -Name Hash -Value $cueHash.Hash
+$checksums += $obj
+
+$metadata | Add-Member -MemberType NoteProperty -Name Checksums -Value $checksums
+
+# Add the dump time
+try
+{
+    $dumpDateTime = Get-Item -Path "$DumpPath\$Game.bin" | Select-Object -ExpandProperty LastWriteTime
+    $metadata | Add-Member -MemberType NoteProperty -Name DumpDateTime -Value $dumpDateTime.ToString('yyyy-MM-ddTHH:mm:sszzz')
+}
+catch
+{
+    Write-Host -Object "Failed retrieving dump date and time from file '$DumpPath\$Game.bin'. Exiting..." -ForegroundColor Red
+    Write-Host -Object $_ -ForegroundColor Red
+    exit
+}
+
+# Exporting to JSON
+try
+{
+    $jsonFilesCount = Get-ChildItem -Path "$DumpPath\*.json" | Measure-Object | Select-Object -ExpandProperty Count
+    $jsonFilePath = $DumpPath + '\' + $game + '.' + ('{0:d3}' -f ($jsonFilesCount+1)) + '.json'
+    $metadata | ConvertTo-Json | Set-Content -Path $jsonFilePath
+}
+catch
+{
+    Write-Host -Object "Failed exporting JSON metadata to file '$jsonFilePath'. Exiting..." -ForegroundColor Red
+    Write-Host -Object $_ -ForegroundColor Red
+    exit
+}
+
+Write-Host -Object "`r`nMetadata exported to '$jsonFilePath'"
+Write-Host -Object ($metadata | ConvertTo-Json)
---
+++ d1-dump.ps1
@@ -0,0 +1,167 @@
+<#
+
+.SYNOPSIS
+    Script to dump Diablo/Hellfire discs with DiscImageCreator.
+
+.DESCRIPTION
+    Script to dump Diablo/Hellfire discs with DiscImageCreator.
+    DiscImageCreator folder path must be in the PATH environment variable.
+    The current directory is used as base path for the dumps.
+
+.PARAMETER DriveLetter
+    The CD-ROM drive letter that will be used to dump the game.
+
+.PARAMETER Game
+    The game being dumped.
+    Accepted values Diablo, Hellfire.
+
+.PARAMETER Platform
+    The game platform.
+    Accepted values PC, PC-Mac, PSX.
+
+.PARAMETER Region
+    The game region.
+    Accepted values NA, EU, PAL, NTSC, NTSC-J.
+
+#>
+Param
+(
+    [Parameter(Mandatory=$true,ValueFromPipeline=$false)]
+    [ValidateSet('A','B','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z')]
+    [String]
+    $DriveLetter,
+    [Parameter(Mandatory=$true,ValueFromPipeline=$false)]
+    [ValidateSet('Diablo','Hellfire')]
+    [String]
+    $Game,
+    [Parameter(Mandatory=$true,ValueFromPipeline=$false)]
+    [ValidateSet('PC','PC-Mac','PSX')]
+    [String]
+    $Platorm,
+    [Parameter(Mandatory=$true,ValueFromPipeline=$false)]
+    [ValidateSet('NA','EU','PAL','NTSC-UC','NTSC-J')]
+    [String]
+    $Region
+)
+
+# Check if CD-ROM is in the drive
+if( -not (Test-Path -Path "$DriveLetter`:") )
+{
+    Write-Host -Object 'Drive not found or CD-ROM not in the drive. Exiting...' -ForegroundColor Red
+    Write-Host -Object $_ -ForegroundColor Red
+    exit
+}
+
+# TODO: Check parameters (e.g. Hellfire PSX is not possible)
+
+# Build dump folder path
+$dumpFolderBasePath = '.'
+$dumpFolderPath = "$dumpFolderBasePath\temp"
+
+try
+{
+    if( -not (Test-Path -Path $dumpFolderPath) )
+    {
+        New-Item -Path $dumpFolderPath -ItemType Directory | Out-Null
+    }
+    else
+    {
+        Remove-Item -Path $dumpFolderPath\* -Recurse -Force -Confirm:$false
+    }
+}
+catch
+{
+    Write-Host -Object 'Failed setting up dump folder. Exiting...' -ForegroundColor Red
+    Write-Host -Object $_ -ForegroundColor Red
+    exit
+}
+
+# Run Disc Image Creator
+Write-Host -Object "Dumping disc..."
+if( $Platorm -eq 'PSX' )
+{
+    DiscImageCreator cd $DriveLetter $dumpFolderPath\$Game.bin 8 /c2 /nl
+}
+else
+{
+    DiscImageCreator cd $DriveLetter $dumpFolderPath\$Game.bin 8 /c2
+}
+
+# If there were errors while dumping remove unused output files
+if( $null -ne (Get-Content -Path "$dumpFolderPath\$Game`_mainError.txt") )
+{
+    Write-Host -Object "Errors found when dumping disc, see '$dumpFolderPath\$Game`_mainError.txt'. Exiting..." -ForegroundColor Red
+    Write-Host -Object $_ -ForegroundColor Red
+    exit
+}
+else
+{
+    Write-Host -Object "Successfully dumped disc to folder '$dumpFolderPath'." -ForegroundColor Green
+}
+
+# Get the volume creation time
+try
+{
+    $volumeCreationDateTimeLine = Get-Content "$dumpFolderPath\$Game`_volDesc.txt" `
+        | Where-Object -FilterScript {$_ -like '*Volume Creation Date and Time:*'} | Select-Object -First 1
+    $volumeCreationDateTimeText = $volumeCreationDateTimeLine.Substring($volumeCreationDateTimeLine.Length-29)
+    $volumeCreationDateTime = [System.DateTimeOffset]::ParseExact($volumeCreationDateTimeText, 'yyyy-MM-dd HH:mm:ss.ff zzz', $null)
+    $volumeCreationDate = $volumeCreationDateTime.UtcDateTime.ToString('yyyy-MM-dd')
+    Write-Host -Object "Volume creation date (UTC): $volumeCreationDate"
+}
+catch
+{
+    Write-Host -Object "Failed retrieving the volume creation time from '$dumpFolderPath\$Game`_volDesc.txt'. Exiting..." -ForegroundColor Red
+    Write-Host -Object $_ -ForegroundColor Red
+    exit
+}
+
+# Get CRC-32 of the disc image
+try
+{
+    $hashLine = Get-Content "$dumpFolderPath\$Game`_disc.txt" | Where-Object -FilterScript {$_ -like "*<rom name=`"$Game.img`"*"}
+    $discImageCrc = $hashLine.Substring($hashLine.IndexOf('crc="')+5,8).ToUpper()
+    Write-Host -Object "Disc image CRC-32 hash: $discImageCrc"
+}
+catch
+{
+    Write-Host -Message "Failed retrieving CRC-32 hash from '$dumpFolderPath\$Game`_disc.txt'. Exiting..." -ForegroundColor Red
+    Write-Host -Object $_ -ForegroundColor Red
+    exit
+}
+
+# Build final dump folder name and path
+$finalDumpFolderName = $Game + '.' + $Platorm + '.' + $Region + '.' + $volumeCreationDate + '.' + $discImageCrc
+$finalDumpFolderPath = "$dumpFolderBasePath\$finalDumpFolderName"
+
+# Check if another dump has the same CRC-32 hash
+Write-Host -Object "Generated disc dump name: $finalDumpFolderName"
+if( Test-Path -Path "$dumpFolderBasePath\*$discImageCrc" -PathType Container )
+{
+    $identicalDump = Get-ChildItem -Path "$dumpFolderBasePath\*$discImageCrc" | Select-Object -ExpandProperty Name
+
+    $existingDiscImageChecksum = Get-FileHash -Path "$dumpFolderPath\$Game.bin"
+    $discImageChecksum = Get-FileHash -Path "$dumpFolderBasePath\$identicalDump\$Game.bin"
+
+    if( $existingDiscImageChecksum.Hash -eq $discImageChecksum.Hash )
+    {
+        Write-Warning -Message "Disc dump with same hash found: $identicalDump"
+        Write-Warning -Message "Exiting..."
+        exit
+    }
+}
+
+# Create dump folder and move .bin + .cue in it
+try
+{
+    New-Item -Path $finalDumpFolderPath -ItemType Directory | Out-Null
+    Move-Item -Path "$dumpFolderPath\$Game.bin","$dumpFolderPath\$Game.cue" -Destination $finalDumpFolderPath
+}
+catch
+{
+    Write-Host -Object "Failed moving dump from '$dumpFolderPath' to '$finalDumpFolderPath'. Exiting..." -ForegroundColor Red
+    Write-Host -Object $_ -ForegroundColor Red
+    exit
+}
+
+Write-Host -Object "Disc dump moved successfully to '$finalDumpFolderPath'." -ForegroundColor Green    
